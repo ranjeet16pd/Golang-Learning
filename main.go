8965ef0b09cb059f169994715d1f4fe22b2133d7
ranjeet16pd <ranjeet16pd@gmail.com>
2024-08-13 11:17:40 +0530

    more about go

--- main.go
+++ main.go
@@ -5,5 +5,11 @@ import "fmt"
 func main() {
 	fmt.Println("Hello World")
 	println("Is golang is really most popular language ")
-	dfsdfdgdfgefg
 }
+
+// Go Created by google
+// It is designed for faster compilation without then need for dependency checking
+// It follows Concurrency : Ability of program to execute multiple tasks independently buy not necessarily simultaneously.
+// Goroutine is a lightweight thread managed by the Go runtime.
+// garbage Collection : in go we do not need to free the memory.
+// Go = C + String + garbage Collection + concurrency
