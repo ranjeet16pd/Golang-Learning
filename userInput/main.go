f2803427955e255372a4645625b3700d70e84940
ranjeet16pd <ranjeet16pd@gmail.com>
2024-08-17 23:00:53 +0530

    learn bufio package

--- userInput/main.go
+++ userInput/main.go
@@ -1,16 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 func main() {
 	fmt.Println("Hey, What is your name ")
-	var name string
+	//var name string
 
 	//it only read until the first whitespace character
-	scan, err := fmt.Scan(&name)
-	if err != nil {
-		return
-	}
-	println(scan)
-	fmt.Printf("Hello , %s!\n", name)
+	//scan, err := fmt.Scan(&name)
+	//if err != nil {
+	//	return
+	//}
+	//println(scan)
+	//fmt.Printf("Hello , %s!\n", name)
+
+	// bufio
+
+	reader, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+	println(reader)
+
 }
